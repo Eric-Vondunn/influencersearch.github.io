ac8a4ba1f7b8c91cdbcd14d21a62827d6b95aad1
Kuanting Pan <pankuanting@gmail.com>
2020-09-16 22:36:49 -0400

    add for loop to test api

--- script.js
+++ script.js
@@ -3,11 +3,12 @@
 // get industy data when click
 $("#industry-button").on("click", function(event) {
     
-
+console.log("123")
   
     event.preventDefault();
-    var industry = $("#industry-button").val();
-    var queryURL = "https://www.omdbapi.com/?t=" + industry + "&apikey=trilogy";
+    // var industry = $("#industry-button").val();
+    // var queryURL = "https://www.omdbapi.com/?t=" + industry + "&apikey=trilogy";
+    var queryURL = "http://www.omdbapi.com/?t=Game%20of%20Thrones&Season=1&&apikey=trilogy";
 
  
     $.ajax({
@@ -16,7 +17,20 @@ $("#industry-button").on("click", function(event) {
     }).then(function(response) {
     console.log(response);
     
-  
+    // get result data
+    for (var i = 0; i < response.Episodes.length; i++){
+    //  append profile info
+    // var getImage = $("<img>").wrap( 
+        // need to update data path
+        // $('<a>').attr("href", response.data[i].Poster )
+       var followerNum = $("<div>").text(response.Episodes[i].imdbRating)
+        $("#topThree").append(followerNum)
+
+
+
+        
+
+    }
 
 
 
